5a38cc6d13a36eb0fe9f231eb911fcec8f59ba35
Nipun <nipungupta593@gmail.com>
2023-06-13 19:41:26 +0530

    Some Fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const handlegreen=()=>{
     <>
       <nav className={`navbar navbar-expand-lg bg-${props.mode}`} data-bs-theme={props.mode}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -23,11 +23,11 @@ const handlegreen=()=>{
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
                 {/* <Link className="nav-link" to="/About">{props.about}</Link> */}
-                <a className="nav-link" href="/">{props.about}</a>
+                <a className="nav-link" href="#">{props.about}</a>
                
               </li>
               {/* <li className="nav-item dropdown">
